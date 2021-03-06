06b3df444bab0b7817a38cafbee6f88af8481ea5
Ivan Martin <57439287+IvanM1903@users.noreply.github.com>
2021-03-06 21:37:05 +0100

    Update usbscan.sh

--- usbscan.sh
+++ usbscan.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-devmon --exec-on-device /dev/sda1 "rsync -r --update localFolder /media/LABEL/" &
+devmon --exec-on-device DEVICE "rsync -r --update LOCAL_FOLDER_TO_BACKUP DEVICE_FOLDER_DESTINATION" &
